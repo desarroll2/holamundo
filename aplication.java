a2306ea91ee826948c51fb4cea57d943c863799b
desarroll2 <38928124+desarroll2@users.noreply.github.com>
2018-05-02 20:56:54 -0300

    Add files via upload

---
+++ aplication.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class aplication here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class aplication
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class aplication
+     */
+    public aplication()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y, int X)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    
+    
+}
